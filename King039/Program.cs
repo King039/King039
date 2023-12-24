4ac58b8f2cefffdd5b046c071ff53aa312335a22
User <User@HOME-PC>
2023-12-24 22:14:29 +0200

    Добавьте файлы проекта.

---
+++ King039/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Two roads diverged in a yellow wood,\r\nAnd sorry I could not travel both\r\nAnd be one traveler, long I stood\r\nAnd looked down one as far as I could\r\nTo where it bent in the undergrowth.");
+Console.WriteLine("1 13 49");
+Console.WriteLine("1  13  49");
