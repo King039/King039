2a0b7b58c55f217273f1cabecded65a8f8897ba2
King039 <bazanks039@gmail.com>
2024-01-20 15:44:26 +0200

    DZ

--- King039/Program.cs
+++ King039/Program.cs
@@ -1,9 +1,6 @@
-//Console.WriteLine("Добиться успеха легко, познать смысл - вот проблема");
-//string str = "Добиться успеха легко, познать смысл - вот проблема";
-//Console.WriteLine("До: " + str);
-//Console.WriteLine("После: " + str.Insert(23, "но "));
-int Moskow = 13015126;
-int Piter = 5598486;
-Console.WriteLine(Math.Abs(Piter - Moskow) / 1000);
+//int Moskow = 13015126;
+//int Piter = 5598486;
+//Console.WriteLine(Math.Abs(Piter - Moskow) / 1000);
+
 double ZP = 260548.56895;
 Console.WriteLine($"{ZP / 12 * 0.13:F2}");
